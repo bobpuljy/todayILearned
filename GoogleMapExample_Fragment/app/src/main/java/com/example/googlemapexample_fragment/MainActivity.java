d11adf87ea3874e849adb6000da512670a65582b
kim junghwan <felixjh84@naver.com>
2020-01-20 13:53:22 +0900

    unfinished fragment, distribution activity and fragment

---
+++ GoogleMapExample_Fragment/app/src/main/java/com/example/googlemapexample_fragment/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.googlemapexample_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            Fragment1 mainFragment = new Fragment1();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.mainFragment, mainFragment, "main")
+                    .commit();
+        }
+    }
+}
