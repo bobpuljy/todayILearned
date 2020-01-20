c2fcb583dc731e25b0cce8e6ab6886945621e2a3
kim junghwan <felixjh84@naver.com>
2020-01-20 13:51:09 +0900

    Activity inner fragment

---
+++ GoogleMapExample/app/src/main/java/com/example/googlemapexample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.googlemapexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.586846, 126.974875);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 7));
+    }
+}
